37bb27bd29ab0c498198d42a771e582735912eb4
fcolabbe <flabbe@eldia.la>
2024-04-29 23:17:45 -0400

    Se agregar nuevas funcionalidades al agregar al carro

--- src/App.jsx
+++ src/App.jsx
@@ -20,26 +20,50 @@ function App() {
   };
 
   const agregarAlCarro = (pizza) => {
+    const existeProducto = carro.find((p) => p.id === pizza.id);
+    if (existeProducto) {
+      const nuevoCarro = carro.map((p) =>
+        p.id === pizza.id ? { ...p, cantidad: p.cantidad + 1 } : p
+      );
+      setCarro(nuevoCarro);
+      setTotal(total + pizza.price);
+      return;
+    }else{
+      pizza.cantidad = 1;
+    }
     setCarro([...carro, pizza])
     setTotal(total + pizza.price)
-/*         console.log(pizza.price)
-    console.log(total) */
-}
+  }
+
+  const eliminarDelCarro = (pizza) => {
+    const existeProducto = carro.find((p) => p.id === pizza.id);
+    if (existeProducto.cantidad > 1) {
+      const nuevoCarro = carro.map((p) =>
+        p.id === pizza.id ? { ...p, cantidad: p.cantidad - 1 } : p
+      );
+      setCarro(nuevoCarro);
+      setTotal(total - pizza.price);
+      return;
+    }
+    const nuevoCarro = carro.filter((p) => p.id !== pizza.id);
+    setCarro(nuevoCarro);
+    setTotal(total - pizza.price);
+  }
 
   useEffect(() => {
     getPizzas();
   }, []);
 
-  const sharedData = { pizzas, setPizzas, carro, setCarro, total, setTotal, agregarAlCarro };
+  const sharedData = { pizzas, setPizzas, carro, setCarro, total, setTotal, agregarAlCarro, eliminarDelCarro };
   return (
     <div>
-      <MiContexto.Provider value={sharedData}>  
+      <MiContexto.Provider value={sharedData}>
         <Navegacion />
         <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path='/carrito' element={<Carrito />} />
-        <Route path="/pizza/:id" element={<Pizza />}></Route>
-        <Route path='*' element={<h1>404 Not Found</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path='/carrito' element={<Carrito />} />
+          <Route path="/pizza/:id" element={<Pizza />}></Route>
+          <Route path='*' element={<h1>404 Not Found</h1>} />
         </Routes>
       </MiContexto.Provider>
     </div>
--- src/vistas/Carrito.jsx
+++ src/vistas/Carrito.jsx
@@ -2,26 +2,26 @@ import MiContexto from "../contexto/MiContexto"
 import { useContext } from "react"
 
 const Carrito = () => {
-  const { carro, setCarro, setTotal, total } = useContext(MiContexto)
+  const { carro, setCarro, setTotal, total, agregarAlCarro, eliminarDelCarro } = useContext(MiContexto)
   return (
     <div className="order-details">
       <h2>Detalles del pedido:</h2>
-      {carro.map((pizza) => (
-        <div className="order-item" key={pizza.id}>
+      {carro.map((pizza, index) => (
+        <div className="order-item" key={`${pizza.id}-${index}`}>
           <img src={pizza.img} alt={pizza.name}></img>
           <div className="order-info">
             <h3>{pizza.name}</h3>
             <p>{pizza.price}</p>
             <div className="quantity-controls">
-              <button className="minus">-</button>
-              <span className="quantity">1</span>
-              <button className="plus">+</button>
+              <button className="minus" onClick={() => eliminarDelCarro(pizza)}>-</button>
+              <span className="quantity">{pizza.cantidad}</span>
+              <button className="plus" onClick={() => agregarAlCarro(pizza)}>+</button>
             </div>
           </div>
         </div>
       ))}
       <div className="total">
-        <h3>$ {total}</h3>
+        <h3>${total}</h3>
         <button className="pay-btn">Ir a Pagar</button>
       </div>
     </div>
